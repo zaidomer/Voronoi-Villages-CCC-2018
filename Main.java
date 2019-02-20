cca02275cacf45397ea4a517bba77e36d5542baf
Zaid Omer <zaidomerone@gmail.com>
2019-02-19 20:47:03 -0500

    Final distances calculations and output

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ public class Main {
         Scanner input = new Scanner(System.in);
         int numVillages = input.nextInt();
         int tempStore = 0;
+        double lowest =  0;
         int[] villageLocation = new int[numVillages];
         for (int i = 0; i < numVillages; i++) {
             villageLocation[i] = input.nextInt();
@@ -20,8 +21,26 @@ public class Main {
             }
         }
 
-        for (int i = 0; i < numVillages; i++) {
-            System.out.print(villageLocation[i] + ", ");
+        // for (int i = 0; i < numVillages; i++) {
+        //     System.out.print(villageLocation[i] + ", ");
+        // }
+
+        // for(int i = 1; i < numVillages-1; i++) {
+        //     double neighbourhoodSize = ((villageLocation[i] - villageLocation[i-1])/2.0) + ((villageLocation[i+1] - villageLocation[i])/2.0);
+        //     if(neighbourhoodSize < lowest || lowest == 0) {
+        //         lowest = (double)Math.round(neighbourhoodSize * 10)/10; 
+        //     }
+        // }
+
+        // System.out.println(lowest);
+
+        for(int i = 1; i < numVillages-1; i++) {
+            double neighbourhoodSize = ((villageLocation[i] - villageLocation[i-1])/2.0) + ((villageLocation[i+1] - villageLocation[i])/2.0);
+            if(neighbourhoodSize < lowest || lowest == 0) {
+                lowest = neighbourhoodSize;
+            }
         }
+
+        System.out.println(String.format("%.1f", lowest));
     }
 }
